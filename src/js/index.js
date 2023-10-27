fdadbc0870bf6f1e6da27cb68435c96dfac6d40e
Artem Morgunov <martinidance@mail.ru>
2023-10-27 21:17:44 +0300

    сделал половину html разметки с клпссами

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
 
 console.log("sfgergergergrege")
 console.log("sfgergergergrege")
